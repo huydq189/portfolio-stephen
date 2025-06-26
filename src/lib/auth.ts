30689a5bd75dce2e1cbc8765164c1c642da074be
Huy Do <huy.doq@one-line.com>
2025-06-26 19:27:43 +0700

    init source

---
+++ src/lib/auth.ts
@@ -0,0 +1,39 @@
+import NextAuth from 'next-auth';
+import GitHub from 'next-auth/providers/github';
+import {DefaultSession} from 'next-auth';
+
+import {ENV} from './constants';
+
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession['user'];
+  }
+}
+
+export const {
+  handlers: {GET, POST},
+  auth,
+} = NextAuth({
+  secret: ENV.AUTH_SECRET,
+  adapter: PrismaAdapter(db),
+  providers: [
+    GitHub({
+      clientId: ENV.GITHUB_CLIENT_ID ?? '',
+      clientSecret: ENV.GITHUB_CLIENT_SECRET ?? '',
+    }),
+  ],
+  callbacks: {
+    session: ({session, user}) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
+  pages: {
+    error: '/',
+  },
+});
